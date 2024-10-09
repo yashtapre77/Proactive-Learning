4fbfca4b130795b8031d21795318a25a4bb9fc02
Yash Tapre <yashtapre77@gmail.com>
2024-10-09 21:55:57 +0530

    reinitialised

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,34 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Main/Navbar'
-import Footer from './components/Footer'
-import { Outlet } from 'react-router-dom'
-import { Link } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
-
-      <header className="sticky top-0">
-        <Navbar/>
-      </header>
-
-      <main>
-        <Outlet/>
-      </main>
-
-      <footer>
-        <Footer/>
-        
-      </footer>
-    </div>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,22 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
-import Home from './components/Main/Home.jsx'
-import Students from './components/Main/Students.jsx'
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<App/>}>
-      <Route path='home' element={<Home/>}/>
-      <Route path='students' element={<Students/>}/>
-      
-    </Route>
-  )
-)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <App />
   </StrictMode>,
 )
