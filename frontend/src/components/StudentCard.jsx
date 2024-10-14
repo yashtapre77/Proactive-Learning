7a987546a854935859c4aea40f5feae213d1dace
Prathmesh_h <xyz@gmail.com>
2024-10-14 11:39:23 +0530

    Added Students Page

---
+++ frontend/src/components/StudentCard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './StudentCard.css';
+
+function StudentCard(props) {
+    return (
+        <div className="cbody">
+
+            <div className="name">
+            <img src="#" alt="Student" className="student-img" />
+                <p>{props.fname} {props.lname}</p>
+            </div>
+
+            <div className="btns">
+                <button className="utilbtns">Edit</button>
+                <button className="utilbtns">Performance</button>
+            </div>
+        </div>
+    );
+}
+
+export default StudentCard;
